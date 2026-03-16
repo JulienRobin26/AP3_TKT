6ad59bf513069c8531e9adafbeabf02e9e86fa5c
alexisdejean <alexis.dejean@campuscarloacutis.fr>
2026-03-16 13:27:50 +0100

    download_api_requirements, creation_components_Nav_Footer_Msg, prepare_e

---
+++ AP3_TKT/API/server.js
@@ -0,0 +1,38 @@
+require('dotenv').config()
+
+const express = require('express')
+const cors = require('cors')
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+
+// ROUTE DE TEST
+
+app.get('/', (req, res) => {
+  res.send('Ceci est une page API de tests')
+})
+
+app.get('/api/message', (req, res) => {
+    infos = {
+        nom: 'Alexis Déjean',
+        age: "19 ans",
+        profession: 'Développeur Web novice',
+    
+    }
+  res.send('Bonjour les gens, je suis un développeur novice et je suis en train de créer une API avec Express.js !' + '<br></br>' + JSON.stringify(infos));
+});
+
+const PORT = process.env.API_PORT || 3000
+app.listen(PORT, () => {
+  console.log('Server is running on http://localhost:' + PORT)
+})
+
+app.get('/api/infos', (req, res) => {
+    informations = {
+        nom: 'Alexis Déjean',
+        projet: "API AP3_TKT",
+        description: "Ceci est une API de test pour l'AP Disney"
+    }
+    res.send(JSON.stringify(informations));
+})
--- AP3_TKT/src/components/includes.jsx
+++ AP3_TKT/src/components/includes.jsx
@@ -1,6 +1,58 @@
-function Nav(){
-    
+﻿import React from 'react';
+
+function Nav({ user }) {
+  return (
+    <>
+      <div className='nav'>
+        <img src="" alt="Logo_Disney" className='logo' />
+        <ul className='liens'>
+          {user.auth === 'admin' ? (
+            <>
+              <li><a href="/gestion_users">Gestions Users</a></li>
+              <li><a href="/gestion_missions">Gestion Missions</a></li>
+              <li><a href="/avertissement">Avertissements</a></li>
+              <li><a href="/deconnexion">Déconnexion</a></li>
+              <li><a href="/profil">Profil</a></li>
+            </>
+          ) : user.auth === 'user' ? (
+            <>
+              <li><a href="/attractions">Les attractions</a></li>
+              <li><a href="/mes_missions">Mes Missions</a></li>
+              <li><a href="/avertissement">Avertissement</a></li>
+              <li><a href="/deconnexion">Déconnexion</a></li>
+              <li><a href="/profil">Profil</a></li>
+            </>
+          ) : null}
+        </ul>
+      </div>
+    </>
+  );
 }
 
-function Footer(){
+const Footer = () => {
+  return (
+    <>
+      <div className='footer'>
+        <ul>
+          <li><a href="/mentions_legales">Mentions légales</a></li>
+          <li><a href="/contact">Contact</a></li>
+          <li><a href="/politique_de_confidentialite">Politique de confidentialité</a></li>
+        </ul>
+        <p>Toutes les images sont la propriété de Disney.</p>
+        <img src="" alt="logo_disney" className='logo' />
+      </div>
+    </>
+  );
+};
+
+function Message({ page }) {
+  return (
+    <>
+      <div className='message_perso'>
+        <h2>{page.nom}</h2>
+      </div>
+    </>
+  );
 }
+
+export { Nav, Footer, Message };
