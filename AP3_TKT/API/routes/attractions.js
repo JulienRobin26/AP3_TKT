ec7210ec7c69c593bd27f3510e1fb68cb7fdcda3
alexisdejean <alexis.dejean@campuscarloacutis.fr>
2026-03-22 23:08:25 +0100

    securisation mineure de la route attractions

--- AP3_TKT/API/routes/attractions.js
+++ AP3_TKT/API/routes/attractions.js
@@ -3,23 +3,40 @@ const router = express.Router();
 const authToken = require('../auth_token');
 const db = require('../config/db');
 
-router.get('/', async (req, res) => {
+router.get('/', authToken, async (req, res) => {
   try {
-    const [rows] = await db.query('SELECT * FROM `infrastructure`');
+    const [rows] = await db.query('SELECT id_ift, nom_ift, description_ift, image_ift, ouvert, tempsAttente FROM `infrastructure`');
     res.json(rows);
   } catch (error) {
     console.error('Error fetching infrastructure:', error);
-    res.status(500).json({ error: 'Internal Server Error' });
+    
+    return res.status(500).json({ error: 'Internal Server Error' });
   }
 });
-router.get('/:id', async (req, res) => {
+router.get('/parc/:id', authToken, async (req, res) => {
   try {
-    const [rows] = await db.query('SELECT * FROM `infrastructure` WHERE id_prc_ift = ?', [req.params.id]);
-    res.json(rows);
+    const [rows] = await db.query('SELECT  id_ift, nom_ift, description_ift, image_ift, ouvert, tempsAttente FROM `infrastructure` WHERE id_prc_ift = ?', [req.params.id]);
+    return res.json(rows);
   } catch (error) {
     console.error('Error fetching infrastructure:', error);
-    res.status(500).json({ error: 'Internal Server Error' });
+    return res.status(500).json({ error: 'Internal Server Error' });
   }
+});
+router.get('/attraction/:id', authToken, async (req, res) => {
+  
+  try{
+    const [rows] = await db.query('SELECT id_ift, nom_ift, description_ift, image_ift, ouvert, tempsAttente FROM `infrastructure` WHERE id_ift = ?', [req.params.id]);
+    if (!rows){
+      return ('Attraction innexistante');
+    }
+    return res.json(rows);
+  }
+  catch{
+      console.error('Error fetching infrastructure:', error);
+      return res.status(500).json({ error: 'Internal Server Error' });
+  }
+
+
 });
 
 module.exports = router;
