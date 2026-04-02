35a79a9325ad349e79a0b8f3ee52ee81eef1b30a
alexisdejean <alexis.dejean@campuscarloacutis.fr>
2026-04-02 23:33:42 +0200

    commit

--- AP3_TKT/src/pages/Profil.jsx
+++ AP3_TKT/src/pages/Profil.jsx
@@ -1,5 +1,6 @@
 import { useNavigate} from "react-router-dom";
 import { useEffect, useState } from "react";
+import "./Profil.css";
 function Profil() {
   const navigate = useNavigate();
   const [id_user, setIdUser] = useState(1);
